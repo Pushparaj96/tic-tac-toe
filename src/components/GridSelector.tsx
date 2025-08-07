6cfce12200e0abeef38f0c0fa6a86e1a3737c333
Pushparaj96 <kpushparaj18@gmail.com>
2025-08-07 16:45:33 +0530

    delete: old ui code in GridSelector

--- src/components/GridSelector.tsx
+++ src/components/GridSelector.tsx
@@ -14,44 +14,6 @@ const GridSelector = () => {
   };
 
   return (
-    // <div className="flex flex-col items-center gap-6 py-10 border-winGlow border-2">
-    //   <h1 className="text-xl md:text-2xl font-semibold shadow-md">
-    //     Choose Board
-    //   </h1>
-    //   <div className="flex gap-4">
-    //     {[3, 4].map((size) => (
-    //       <button
-    //         key={size}
-    //         disabled={gameStarted}
-    //         onClick={() => handleSelect(size as 3 | 4)}
-    //         className={`px-6 py-3 rounded-lg text-lg font-semibold border-2 transition-all duration-200
-    //                 ${
-    //                   gridSize === size
-    //                     ? "border-blue-500 bg-blue-100 dark:bg-blue-800"
-    //                     : "border-gray-400"
-    //                 }
-    //                 ${
-    //                   gameStarted
-    //                     ? "cursor-not-allowed opacity-50"
-    //                     : "hover: border-blue-500"
-    //                 }
-    //                 `}
-    //       >
-    //         {size} x {size}
-    //       </button>
-    //     ))}
-    //   </div>
-
-    //   <button
-    //     disabled={gameStarted}
-    //     onClick={startGame}
-    //     className={`mt-4 px-6 py-3 rounded-lg text-white text-lg font-bold bg-green-500
-    //     hover:bg-green-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed
-    //     `}
-    //   >
-    //     Start Game
-    //   </button>
-    // </div>
     <div className="flex items-center justify-center min-h-[60vh] p-4">
       <div className="bg-bgColor border-2 border-winGlow rounded-2xl shadow-lg p-8 md:p-12 max-w-md w-full">
         <div className="flex flex-col items-center gap-8">
